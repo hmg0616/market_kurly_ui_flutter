1a49d6525babfd3933dd765e973e067ea19e49d9
mungyu <mungyu83@gmail.com>
2023-07-20 01:14:29 +0900

    스플래시 페이지 개발

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:market_kurly_ui/constants.dart';
+
+import '../main_screens.dart';
 
 class SplashScreen extends StatelessWidget {
 
@@ -6,10 +10,41 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('SplashScreen'),
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: SafeArea( // 자식 위젯에 패딩을 넣어서 디바이스 영역이 앱의 위젯 영역을 침범하는 것을 막아준다.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: Text(
+                  "Kurly",
+                  style: GoogleFonts.pacifico(fontSize: 28, color: Colors.white),
+                ),
+              )
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.all(24),
+                child: TextButton(
+                  child: Text("Continue"),
+                  onPressed: () {
+                    _completeSplash(context, MainScreens());
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  void _completeSplash(BuildContext context, Widget widget) {
+    Navigator.pushReplacement( // 이전 경로를 없애 주면서 새로운 화면으로 이동
+        context, MaterialPageRoute(builder: (BuildContext context) => widget));
   }
 }
