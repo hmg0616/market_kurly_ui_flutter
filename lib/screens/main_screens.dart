352b70b8ebf022cda42df435582915e322c8437b
mungyu <mungyu83@gmail.com>
2023-07-20 01:24:51 +0900

    StatelessWidget -> StatefulWidget 변경

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MainScreens extends StatelessWidget {
+class MainScreens extends StatefulWidget { // 화면이 변경되어야 하는 위젯이기 떄문에 StatefulWidget 사용
 
   static String routeName = "/main_screens";
 
+  @override
+  _MainScreensState createState() => _MainScreensState();
+}
+
+class _MainScreensState extends State<MainScreens> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
