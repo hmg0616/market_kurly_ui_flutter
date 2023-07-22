8a201afc69a47cbaffedef0c48d2bd90beb9ee01
mungyu <mungyu83@gmail.com>
2023-07-22 10:44:06 +0900

    마이컬리 메뉴 - 회원가입버튼 및 텍스트메뉴 리스트 (SingleChildScrollView)

--- lib/screens/my_kurly/my_kurly_screen.dart
+++ lib/screens/my_kurly/my_kurly_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:market_kurly_ui/screens/components/custom_actions.dart';
 
+import 'components/my_kurly_body.dart';
+
 class MyKurlyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFFF5F5F5),
       appBar: AppBar(
         automaticallyImplyLeading: false, // 왼쪽 뒤로기가 버튼 없애기
         title: Text('마이컬리'),
         actions: [CustomActions()],
       ),
-      body: Center(
-        child: Text("MyKurlyScreen"),
+      body: SingleChildScrollView(
+        child: MyKurlyBody(),
       ),
     );
   }
