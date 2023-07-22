96426f7a30fa65adbffb85cc2b4daf748d60c829
mungyu <mungyu83@gmail.com>
2023-07-22 09:35:50 +0900

    마이컬리 메뉴 - AppBar

--- lib/screens/my_kurly/my_kurly_screen.dart
+++ lib/screens/my_kurly/my_kurly_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/screens/components/custom_actions.dart';
 
 class MyKurlyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // 왼쪽 뒤로기가 버튼 없애기
+        title: Text('마이컬리'),
+        actions: [CustomActions()],
+      ),
       body: Center(
         child: Text("MyKurlyScreen"),
       ),
