ab8ae9c636e67e84c474dc9db3cdb9e0ef234d3f
mungyu <mungyu83@gmail.com>
2023-07-27 01:07:07 +0900

    카테고리 메뉴 - 앱바 및 단일 위젯 생성 (CustomScrollView - SliverToBoxAdapter)

--- lib/screens/category/category_screen.dart
+++ lib/screens/category/category_screen.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:market_kurly_ui/constants.dart';
+
+import '../components/custom_actions.dart';
+import '../components/text_menu_card.dart';
 
 class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("CategoryScreen"),
+      appBar: AppBar(
+        title: Text("카테코리"),
+        actions: [CustomActions()],
+        automaticallyImplyLeading: false,
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverPadding( // CustomScrollView안에 단일 위젯을 생성할 때는 SliverToBoxAdapter를 사용하는것이 좋음
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            sliver: SliverToBoxAdapter(
+              child: SizedBox(
+                height: 60,
+                child: TextMenuCard(
+                  title: "자주 사는 상품",
+                  icon: "assets/icons/right-arrow.svg",
+                  textColor: kPrimaryColor,
+                  iconColor: kPrimaryColor,
+                  press: () {},
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
